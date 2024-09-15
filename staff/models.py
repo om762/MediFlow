e84eb90d62981461b13dc3c8831c0d2e07c9491e
om762 <omprakash824rawat@gmail.com>
2024-09-15 11:07:39 +0530

    Frontend code to templates, complete
    Some testing remaining

--- staff/models.py
+++ staff/models.py
@@ -1,28 +1,28 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-class Hospital(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.TextField()
-    contact_number = models.CharField(max_length=15)
-    email = models.EmailField()
+# class Hospital(models.Model):
+#     name = models.CharField(max_length=200)
+#     address = models.TextField()
+#     contact_number = models.CharField(max_length=15)
+#     email = models.EmailField()
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
-class Staff(AbstractUser):
-    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    contact_number = models.CharField(max_length=15)
+# class Staff(AbstractUser):
+#     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+#     contact_number = models.CharField(max_length=15)
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
-class Doctor(Staff):
-    specialization = models.CharField(max_length=100, blank=True, null=True)
-    is_specialist = models.BooleanField(default=False)
+# class Doctor(Staff):
+#     specialization = models.CharField(max_length=100, blank=True, null=True)
+#     is_specialist = models.BooleanField(default=False)
 
-    class Meta:
-        verbose_name = 'Doctor'
-        verbose_name_plural = 'Doctors'
+#     class Meta:
+#         verbose_name = 'Doctor'
+#         verbose_name_plural = 'Doctors'
 
